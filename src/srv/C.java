ceffc90aeae012bcc7ede69dc7d1b4a3b1822775
ValentinaChaconBuitrago <v.chacon@uniandes.edu.co>
2019-04-30 20:53:11 -0500

    cambios

--- src/srv/C.java
+++ src/srv/C.java
@@ -22,12 +22,12 @@ public class C {
 	private static final String MAESTRO = "MAESTRO: ";
 	private static X509Certificate certSer; /* acceso default */
 	private static KeyPair keyPairServidor; /* acceso default */
-	
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) throws Exception{
-		
+
 		System.out.println(MAESTRO + "Establezca puerto de conexion:");
 		InputStreamReader isr = new InputStreamReader(System.in);
 		BufferedReader br = new BufferedReader(isr);
@@ -36,17 +36,17 @@ public class C {
 		// Adiciona la libreria como un proveedor de seguridad.
 		// Necesario para crear llaves.
 		Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());		
-		
+
 		int idThread = 0;
 		// Crea el socket que escucha en el puerto seleccionado.
 		ss = new ServerSocket(ip);
 		System.out.println(MAESTRO + "Socket creado.");
-		
+
 		keyPairServidor = S.grsa();
 		certSer = S.gc(keyPairServidor);
 		D.initCertificate(certSer, keyPairServidor);
 		ExecutorService executor = Executors.newFixedThreadPool(1);
-		
+
 		// Blank workbook 
 		HSSFWorkbook workbook = new HSSFWorkbook();
 		System.out.println("Ingrese el numero de pruebas que va a realizar");
@@ -55,35 +55,32 @@ public class C {
 		//1) intentar con 800 peticiones
 		//2) intentar con 300 peticiones
 		//3) intentar con 100 peticiones
-		int counter = 1;
-		while(takes >= counter) {
-			Map<String, Object[]> data = new TreeMap<String, Object[]>(); 
-			data.put("1", new Object[]{ "NAME", "LASTNAME" }); 
-			System.out.println("Ingrese el numero de peticiones que va a realizar para la prueba # " + counter);
-			int peticiones = Integer.parseInt(br.readLine());
-			int kkey = 2;
-			
-			//Realice 800 peticiones
-			while (peticiones > 0) {
-				try { 
-					Socket sc = ss.accept();
-					System.out.println(MAESTRO + "Cliente " + idThread + " aceptado.");
-					executor.submit(new D(sc, idThread));
-					idThread++;
-					String f = ""+kkey;
-					//TODO aqui se crea un objeto con los resultados de las medidas de respuesta tomadas en la peticion
-					data.put(f, new Object[]{ "CHACON", "Kumar" }); 
-					kkey++;
-					peticiones--;
-				} catch (IOException e) {
-					System.out.println(MAESTRO + "Error creando el socket cliente.");
-					e.printStackTrace();
-				}
+
+		Map<String, Object[]> data = new TreeMap<String, Object[]>(); 
+		data.put("1", new Object[]{ "NAME", "LASTNAME" }); 
+		System.out.println("Ingrese el numero de peticiones que va a realizar para la prueba");
+		int peticiones = Integer.parseInt(br.readLine());
+		int kkey = 2;
+		//Realice 800 peticiones
+		while (peticiones > 0){
+			try { 
+				Socket sc = ss.accept();
+				System.out.println(MAESTRO + "Cliente " + idThread + " aceptado.");
+				System.out.println("se creo un DD");
+
+				executor.submit(new D(sc, idThread));
+				idThread++;
+				String f = ""+kkey;
+				//TODO aqui se crea un objeto con los resultados de las medidas de respuesta tomadas en la peticion
+				data.put(f, new Object[]{ "CHACON", "Kumar" }); 
+				kkey++;
+				peticiones--;
+			} catch (IOException e) {
+				System.out.println(MAESTRO + "Error creando el socket cliente.");
+				e.printStackTrace();
 			}
-			ExcelFile.generate(workbook, counter, data);
-			counter++;;
 		}
-				
+		ExcelFile.generate(workbook, 1, data);				
 		try { 
 			// this Writes the workbook gfgcontribute 
 			System.out.println("Ingrese el nombre del archivo donde desea almacenar los resutados ");
