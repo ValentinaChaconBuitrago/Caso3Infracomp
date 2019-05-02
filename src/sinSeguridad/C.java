0b02621a4d5c8463a5afca5e0ff71c358d18f6b5
ValentinaChaconBuitrago <v.chacon@uniandes.edu.co>
2019-05-02 12:19:56 -0500

    refactor

--- src/sinSeguridad/C.java
+++ src/sinSeguridad/C.java
@@ -1,4 +1,4 @@
-package srv;
+package sinSeguridad;
 
 import java.io.BufferedReader;
 import java.io.IOException;
--- src/sinSeguridad/S.java
+++ src/sinSeguridad/S.java
@@ -1,4 +1,4 @@
-package srv;
+package sinSeguridad;
 
 import java.math.BigInteger;
 import java.security.KeyPair;
